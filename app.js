50e3ec2ad4cc2b4fb752d3145a548c73743305d1
hysdx12345 <245406458@qq.com>
2019-10-10 12:02:47 +0800

    网页上传

--- app.js
+++ app.js
@@ -3,41 +3,82 @@ const async = require('async');
 const superagent = require('superagent');
 require('superagent-charset')(superagent);
 const cheerio = require('cheerio');
-const express = require('express');
 const url = require('url');
+const fs = require('fs');
+const express = require('express');
 
 const app = express();
+// const mongoose = require('mongoose');
+
+// const Murl = "mongodb://localhost:27017/book";
+
+
+// mongoose.connect(Murl);
+// const con = mongoose.connection;
+
+// con.on('error', console.error.bind(console, '连接数据库失败'));
+// con.once('open', () => {
+//             //成功连接
+//             //定义一个schema
+//             let bookSchema = mongoose.Schema({
+//                 title: String,
+//                 content: String
+//             });
+//             bookSchema.methods.read = function() {
+//                 console.log("已保存：" + this.title);
+//             }
+//             //继承一个schema
+//             let bookModel = mongoose.model("book", bookSchema);
+
+//         })
 
 const allbookUrl = 'http://www.quanshuwang.com/book/0/269';
 
-
-
 const fetchUrl = function(topicUrl, callback) {
-    console.log('正在抓取' + topicUrl)
+    console.log('正在抓取：' + topicUrl)
     superagent.get(topicUrl)
         .buffer('true')
         .charset('gbk')
         .then((res, err) => {
             let $ = cheerio.load(res.text);
-
             let title = $('.jieqi_title').text().trim();
             let content = $('.mainContenr').text();
             content = content.replace(/\s*/g, "");
 
             result = {
-                title: title,
-                content: content
+                "title" : title,
+                "content" : content
             };
+
+			// con.on('error', console.error.bind(console, '连接数据库失败'));
+			// con.once('open', () => {
+			//     //成功连接
+			//     //生成一个document
+	  //           let book = new bookModel({
+	  //               title: title,
+	  //               content: content
+	  //           });
+
+			//     //存放数据
+			//     book.save((err, book) => {
+			//         if (err) return console.log(err);
+			//         apple.read();
+			//         //查找数据
+			//         Model.find({ name: 'book' }, (err, data) => {
+			//             console.log(data);
+			//         })
+			//     });
+			// })
+
             callback(err, result);
         })
 }
-
 superagent.get(allbookUrl)
     .buffer('true')
     .charset('gbk')
     .then((res, err) => {
         if (err) {
-            return console.error(err);
+            console.error(err);
         }
         let topicUrls = [];
         let $ = cheerio.load(res.text);
@@ -56,36 +97,25 @@ superagent.get(allbookUrl)
             console.log(err);
         }
 
-        let urls = res.slice(0,50);
+        let urls = res.slice(0, 50);
 
-
-
-        async.mapLimit(urls, 5, function(url, callback) {
+        async.mapLimit(urls, 10, function(url, callback) {
             fetchUrl(url, callback);
-        }, (err, res) => {
+        }, function(err, results) {
             if (err) {
                 console.log(err);
             }
-            console.log('最终抓取数据：');
-            console.log(res);
 
-
-
-
-            let items = res;
-
-            app.get('/', function(req, res, next) {
-
-                res.send(items);
-
-            });
-
+            app.get('/', function (req, res, next) {
+            	// body...
+            	res.send(result);
+            })
             let port = process.env.PORT;
-            if (port == null || port == "") {
-                port = 4000;
-            }
-            app.listen(port,function () {
-                console.log('app is listening at port'+ port);
-            });
+			if (port == null || port == "") {
+			  port = 3000;
+			}
+			app.listen(port);
+            // console.log('最终抓取数据: ');
+            // console.log(results);
         })
     })
