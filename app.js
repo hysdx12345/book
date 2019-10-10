e25a053f11949ceea0405c6a213e1c7a975a89d4
hysdx12345 <245406458@qq.com>
2019-10-10 15:20:45 +0800

    修改返回数量

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ superagent.get(allbookUrl)
             console.log(err);
         }
 
-        let urls = res.slice(0,100);
+        let urls = res.slice(0,3000);
 
         async.mapLimit(urls, 10, function(url, callback) {
             fetchUrl(url, callback);
