8e61ed9556983aa2803626c2463560695a964609
hysdx12345 <245406458@qq.com>
2019-11-06 23:25:48 +0800

    修改章节对应

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ superagent.get(allbookUrl)
         app.get('/fr',function (req, res){
             //通过get方法获取抓取小说范围
             let start = +req.query.start-1||0;
-            let end = +req.query.end||(+req.query.start+1)||30;
+            let end = +req.query.end||(+req.query.start)||30;
             console.log(start,end);
             urls = allTopicUrls.slice(start,end);
             async.mapLimit(urls, 10, function(url, callback) {
